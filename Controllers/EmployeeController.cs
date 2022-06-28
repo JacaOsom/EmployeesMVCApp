83e8ad844e869d8e6d39440e8b681526d7d1619b
JacaOsom <jacekfilipski@gmail.com>
2022-06-28 11:59:14 +0200

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,87 @@
+﻿using EmployeesMVCApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesMVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static IList<EmployeeModel> employees = new List<EmployeeModel>()
+        {
+            new EmployeeModel(){ EmployeeId = 1, Name = "Jacek", Lastname = "Filipski", Age = 27, DateOfHired = new DateTime(2019, 04, 11), JobPosition = "CEO", PhoneNumber = "721066887", 
+                Email = "jckfksi@email.com", City = "Gdańsk", ZIPCode = "80-174", Street = "Potęgowska", BuildingNumber = "20", ApartmentNumber = "4"},
+            new EmployeeModel(){ EmployeeId = 2, Name = "Bartek", Lastname = "Nadolny", Age = 22, DateOfHired = new DateTime(2020, 03, 01), JobPosition = "Assistant", PhoneNumber = "758458963",
+                Email = "bartasgsist@email.com", City = "Gdańsk", ZIPCode = "80-180", Street = "Kolorowa", BuildingNumber = "4", ApartmentNumber = null}
+        };
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View(new EmployeeModel());
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EmployeeModel employee)
+        {
+            employee.EmployeeId = employees.Count + 1;
+            employees.Add(employee);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesMVCApp.Models
+{
+    public class EmployeeModel
+    {
+        //[Key]
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfHired { get; set; }
+        public string JobPosition { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get;set; }
+        public string City { get;set; }
+        public string ZIPCode { get;set; }
+        public string Street { get;set; }
+        public string BuildingNumber { get;set; }
+        public string ApartmentNumber { get;set; }
+
+    }
+}
